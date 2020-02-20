05f142e0c2e944b058e79545c7282113a941bb02
hruth9 <ruthholmes92@gmail.com>
2020-02-20 09:28:21 +0000

    Change to engine.execute

--- Flask app/app.py
+++ Flask app/app.py
@@ -12,30 +12,16 @@ app = Flask(__name__)
 engine = create_engine("mysql://admin:SET14GP2020@dublinbikes.c69eptjjnovd.us-east-1.rds.amazonaws.com:3306/dublinbikes")
 engine.connect()
 
-test_data = [
-
-    {
-        'location': 'Capel Street',
-        'num_bikes': '12',
-        'weather': 'not great my dude'
-    },
-    {
-        'location': 'Parnell Street',
-        'num_bikes': '3',
-        'weather': 'not great here either bruh'
-    }
-]
-
-
 @app.route("/")
 @app.route("/home")
 def dub_bikes():
-    return render_template('home.html', posts=test_data)  # pulls home.html template from templates folder
+    l1 = engine.execute('select name from static_data')
+    return render_template('home.html', l1=l1)  # pulls home.html template from templates folder
 
 
-@app.route("/about")  # added for testing purposes. Will be helpful if we needed to add any extra pages later on
-def about():
-    return render_template('home.html', title='About')
+#@app.route("/about")  # added for testing purposes. Will be helpful if we needed to add any extra pages later on
+#def about():
+#    return render_template('home.html', title='About')
 
 
 # allows us to run directly with python i.e. don't have to set env variables each time
