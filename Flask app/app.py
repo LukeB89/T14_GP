8232265ae1093e4396b4bbc443efdd5809c3bf0a
hruth9 <ruthholmes92@gmail.com>
2020-03-03 11:50:36 +0000

    Map API, map added to webapp

--- Flask app/app.py
+++ Flask app/app.py
@@ -4,7 +4,7 @@ Created on 16 Feb 2020
 @author: Ruth Holmes
 '''
 
-from flask import Flask, render_template
+from flask import Flask, render_template, g, jsonify
 from sqlalchemy import create_engine
 
 app = Flask(__name__)
@@ -12,16 +12,15 @@ app = Flask(__name__)
 engine = create_engine("mysql://admin:SET14GP2020@dublinbikes.c69eptjjnovd.us-east-1.rds.amazonaws.com:3306/dublinbikes")
 engine.connect()
 
-@app.route("/")
-@app.route("/home")
+@app.route("/test")
 def dub_bikes():
-    l1 = engine.execute('select name from static_data')
+    l1 = engine.execute('select name from static_data')  #
     return render_template('home.html', l1=l1)  # pulls home.html template from templates folder
 
 
-#@app.route("/about")  # added for testing purposes. Will be helpful if we needed to add any extra pages later on
-#def about():
-#    return render_template('home.html', title='About')
+@app.route("/")
+def bikemap():
+    return render_template('map.html', title='Map')
 
 
 # allows us to run directly with python i.e. don't have to set env variables each time
