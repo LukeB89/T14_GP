6dac4618ae6635b9dcbd33b5d4c99c0c92c5f7f2
MBashford <54007477+MBashford@users.noreply.github.com>
2020-03-18 17:39:12 +0000

    update function "index_page" to request co-ordinates, name, number & dynamic bikes/weather data for each bike station from DataBase and render this to the index.html template

--- Flask app/app.py
+++ Flask app/app.py
@@ -19,11 +19,23 @@ engine = create_engine("mysql://" + options["user"] + ":" + options["passwd"] + 
                        + options["host"] + "/" + options["database"])
 engine.connect()
  
-  
+# @app.route("/test")
+# def dub_bikes():
+#     l1 = engine.execute('select name from static_data')  #
+#     return render_template('home.html', l1=l1)  # pulls home.html template from templates folder
+#  
 @app.route("/index")
 def index_page():
-    statinfo = engine.execute('select number, address, lat, lng from static_data')
+    # request co-ordinates, name, number & dynamic bikes/weather data from DataBase for each bike station
+    statinfo = engine.execute("""
+        select s.number, s.address, b.status, b.available_bike_stands, b.available_bikes, 
+            w.weather_main, w.weather_icon, w.main_temp, w.main_feels_like, w.rain_1h,
+            s.lat, s.lng 
+        from static_data s, bikes_current b, weather_current w, bike_weather_assoc a
+        where s.number = b.number and b.number = a.bike_station_id and a.weather_station = w.name
+        """)
     return render_template('index.html', title='Map', statinfo=statinfo)
+
 
 @app.route("/index/route")
 def index_page_route():
@@ -31,11 +43,13 @@ def index_page_route():
     coords = {"dest":[float(request.args.get('tolat')),float(request.args.get('tolong'))],
             "origin":[float(request.args.get('fromlat')), float(request.args.get('fromlong'))]}
     return render_template('routeindex.html', title='Map', statinfo=statinfo, coordinates=coords)
-  
+
+
 @app.route("/info")
 def info_page():
     stat_addr = engine.execute('select number, address from static_data')
     return render_template('info.html', stat_addr = stat_addr, stat_info="none")  # pulls home.html template from templates folder
+
 
 @app.route("/info/<stat_id>")
 def info_page_refined(stat_id):
@@ -49,10 +63,12 @@ def bikemap():
     statinfo = engine.execute('select number, address, lat, lng from static_data')
     return render_template('map.html', title='Map', statinfo=statinfo)
 
+
 @app.route("/route")
 def routemap():
     statinfo = engine.execute('select number, address, lat, lng from static_data')
     return render_template('route.html', title='Route', statinfo=statinfo)
+
 
 # allows us to run directly with python i.e. don't have to set env variables each time
 if __name__ == '__main__':
