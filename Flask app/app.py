b0c209353b38c48bab3b0e312c884664a4bc4ac1
Luke Byrne <lukebyrne89@gmail.com>
2020-03-19 17:24:27 +0000

    Front End Update: Encorporated the route planning into the front end

--- Flask app/app.py
+++ Flask app/app.py
@@ -4,7 +4,7 @@ Created on 16 Feb 2020
 @author: Ruth Holmes
 '''
 
-from flask import Flask, render_template, g, jsonify
+from flask import Flask, render_template, g, jsonify, request
 from sqlalchemy import create_engine
 from configparser import ConfigParser
 
@@ -19,15 +19,18 @@ engine = create_engine("mysql://" + options["user"] + ":" + options["passwd"] + 
                        + options["host"] + "/" + options["database"])
 engine.connect()
  
-# @app.route("/test")
-# def dub_bikes():
-#     l1 = engine.execute('select name from static_data')  #
-#     return render_template('home.html', l1=l1)  # pulls home.html template from templates folder
-#  
+  
 @app.route("/index")
 def index_page():
     statinfo = engine.execute('select number, address, lat, lng from static_data')
     return render_template('index.html', title='Map', statinfo=statinfo)
+
+@app.route("/index/route")
+def index_page_route():
+    statinfo = engine.execute('select number, address, lat, lng from static_data')
+    coords = {"dest":[float(request.args.get('tolat')),float(request.args.get('tolong'))],
+            "origin":[float(request.args.get('fromlat')), float(request.args.get('fromlong'))]}
+    return render_template('routeindex.html', title='Map', statinfo=statinfo, coordinates=coords)
   
 @app.route("/info")
 def info_page():
@@ -37,9 +40,7 @@ def info_page():
 @app.route("/info/<stat_id>")
 def info_page_refined(stat_id):
     stat_info = engine.execute('select * from static_data where number = {}'.format(stat_id))
-    print(stat_info)
     stat_addr = engine.execute('select number, address from static_data')
-    print(stat_addr)
     return render_template('info.html', stat_addr = stat_addr,  stat_info = stat_info)  # pulls home.html template from templates folder
 
 
