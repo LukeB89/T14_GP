0fe04a56e1b4dd5a054307d69a0d89664989c69f
MBashford <54007477+MBashford@users.noreply.github.com>
2020-04-16 23:08:39 +0100

    update bikes_main.py - misc cleaning to improve readability & compliance with pep8

--- scraper/bikes_main.py
+++ scraper/bikes_main.py
@@ -1,18 +1,19 @@
-'''
+"""
 Created on 10 Feb 2020
 Version: 0.1
 @author: Luke Byrne
 main method to call scrapers
-'''
+"""
 # Import liberies for use of the  
 import get_bikes_data as bikes
 import get_weather_data as weather
 import csv
 
+
 def main():
     
-#   main code which is tasked with running the JSON Parser
-#   keeps a log to ensure the returned API information doesnt crash the program.
+    #   main code which is tasked with running the JSON Parser
+    #   keeps a log to ensure the returned API information doesnt crash the program.
     
     with open('log.txt', 'a', newline='') as file:
         log_out = csv.writer(file)
@@ -22,5 +23,7 @@ def main():
         
         weather.get_weather_all()
     return
+
+
 if __name__ == '__main__':
     main()
